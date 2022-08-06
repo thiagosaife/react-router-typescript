c61285f680a9067a9aec2f31c48e6d0109177f30
Thiago Saife Rodrigues <ts@storyblok.com>
2022-08-06 12:48:06 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+const Home = React.lazy(() => import('./Pages/Home'))
+const About = React.lazy(() => import('./Pages/About'))
+const PageNotFound = React.lazy(() => import('./Pages/PageNotFound'))
+
+type PathObject = {
+  path: string,
+  name: string,
+  element: JSX.Element,
+}
+interface IPaths {
+  map(arg0: (p: PathObject, i: number) => JSX.Element): React.ReactNode;
+}
 
 function App() {
+  const paths:IPaths = [
+    {
+      path: '/',
+      name: 'Home',
+      element: <Home />,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      element: <About />,
+    },
+    {
+      path: '*',
+      name: 'PageNotFound',
+      element: <PageNotFound />,
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          {
+            paths
+              .map(
+                (p, i) =>
+                  <li key={i}>
+                    <Link to={p.path}>{p.name}</Link>
+                  </li>
+              )
+          }
+        </ul>
+      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {
+            paths
+              .map(
+                (p, i) =>
+                  <Route key={i} path={p.path} element={p.element} />
+                )
+          }
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ src/Pages/PageNotFound.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <p>404 - Page not found</p>
+    </div>
+  )
+}
